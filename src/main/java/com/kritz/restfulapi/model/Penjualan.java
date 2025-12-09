ea4d84a53d93c9e37c263e6d98bb410f0287e5ff
Gilbert Winardy <www.gilbertwinardy@gmail.com>
2025-12-09 14:04:33 +0800

    Update Penjualan.java

--- src/main/java/com/kritz/restfulapi/model/Penjualan.java
+++ src/main/java/com/kritz/restfulapi/model/Penjualan.java
@@ -47,7 +47,7 @@ public class Penjualan {
     @Column(name = "diskon", nullable = false)
     private double diskon;
 
-    @Column(name = "nama_pelanggan", nullable = false, length = 25)
+    @Column(name = "nama_pelanggan", nullable = true, length = 25)
     private String namaPelanggan;
 
     @Enumerated(EnumType.STRING)
